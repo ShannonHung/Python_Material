ec21616fad9df2c6a9f2fce067a731b1b7bf2f35
ShannonHung <hsuanhung036@gmail.com>
2021-08-12 10:11:28 +0800

    0812 add: HW 2 answer

--- HW/HW1_Q2_TeamO_黃曉明.py
+++ HW/HW1_Q2_TeamO_黃曉明.py
@@ -11,22 +11,31 @@ wrong = 0 # 錯誤累積次數
 while True:
     try:
         month = input("請輸入你在幾月出生 (ex. 1), 如果想退出請輸入 q 或 Q =>")
-        # TODO: 如果輸入q或是Q就退出 
-
-            
-        # TODO: 因為要進行數字比較，請轉成int
-        if(# TODO: 判斷month是否在3-5):
+        # DONE: 如果輸入q或是Q就退出 
+        if(month == 'q' or month == 'Q'):
+            break
+        # DONE: 因為要進行數字比較，請轉成int
+        month = int(month)
+        if(month >= 3 and month <= 5):
             print(f'{month}月是在春天')
-            # TODO: 春天的次數+1 
-        # TODO: 夏天, 秋天, 冬天都是類似跟春天差不多作法
-  
-        # TODO: 如果使用者不是輸入1-12月就顯示以下錯誤
-            # TODO: 錯誤累計次數+1次
+            # DONE: 秋天, 夏天, 冬天也是跟春天一樣的步驟
+        elif(month >= 6 and month <= 8):
+            print(f'{month}月是在夏天')
+        elif(month >= 9 and month <= 11):
+            print(f'{month}月是在秋天')
+        # 12 1 2 冬天
+        elif(month == 1 or month == 2 or month == 12):   
+            print(f'{month}月是在冬天')
+        else:
             print("錯誤的範圍，請輸入1-12其中一個數字")
+            wrong += 1 # wrong = wrong + 1 
+        # DONE: 如果使用者不是輸入1-12月就顯示以下錯誤
+            # DONE: 錯誤累計次數+1次
     except:
-        print("請輸入整數，請輸入1-12其中一個數字")
-        # TODO: 如果輸入非整數內容，錯誤累計次數+1次並且重新輸入
-        
+        wrong +=1 # 錯誤累計次數+1次
+        print("!!!WRONG !!!! please input the integer")
+        continue # 重新輸入
+        # DONE: 如果輸入非整數內容，錯誤累計次數+1次 並且 重新輸入
 print(f"春天{answer['春天']}人出生")
 print(f"夏天{answer['夏天']}人出生")
 print(f"秋天{answer['秋天']}人出生")
